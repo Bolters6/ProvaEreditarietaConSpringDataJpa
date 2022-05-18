f44103819bafef01e4b687771a18971687b25565
Bolters6 <96733986+Bolters6@users.noreply.github.com>
2022-05-18 10:36:24 +0200

    Add files via upload

---
+++ ProvaEridetarietaHibernate-master/src/main/java/test/fornitore/repository/FornitoreRepo.java
@@ -0,0 +1,12 @@
+package test.fornitore.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import test.fornitore.model.Fornitore;
+
+public interface FornitoreRepo extends JpaRepository<Fornitore, Long> {
+
+
+    Fornitore findByEmail(String email);
+
+    boolean existsByEmail(String email);
+}
